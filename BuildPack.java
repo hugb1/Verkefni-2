696ad9163c82c30a1cc43faf5dba4ee00ebb7253
PeturLogi <plp2@hi.is>
2017-11-14 23:55:45 +0000

    Create BuildPack.java

---
+++ BuildPack.java
@@ -0,0 +1,17 @@
+/**
+ *
+ * @author Pétur Logi Pétursson
+ * @date september 2017
+ * HBV501G Hugbúnaðarverkefni 1
+ * Háskóli Íslands
+ * 
+ * Meðhöndlar whitelabel villur sem verða á þjóninum.
+ * 
+ */
+ 
+public class BuildPack {
+  
+  public static void main() {
+    
+  }
+}
